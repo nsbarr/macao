e35370fa5e060006fe9b07e3225f1237f9902006
nick barr <nick@nicks-air.westell.com>
2014-07-12 12:46:27 -0400

    Initial Commit

---
+++ Macao/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  Macao
+//
+//  Created by nick barr on 7/12/14.
+//  Copyright (c) 2014 poemsio. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+                            
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
